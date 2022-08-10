66c0af8c6153827957beeb8d9ca0a17aaec4b8e6
ftqo <fittsqo@gmail.com>
2022-08-10 09:06:22 -0700

    add experimental redoc endpoint

--- facade/facade.go
+++ facade/facade.go
@@ -2,6 +2,7 @@ package facade
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
@@ -32,7 +33,46 @@ func Run(wg *sync.WaitGroup, ctx context.Context, log zerolog.Logger, c config.F
 	r := chi.NewRouter()
 	r.Use(middleware.RealIP, middleware.RequestID, middleware.Recoverer, newRequestLogger(log))
 
-	handler := Handler(f, WithRouter(r), WithMiddleware("session", f.sessionMiddleware))
+	r.Get("/v0/redoc", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<!DOCTYPE html>
+		<html>
+		  <head>
+			<title>Redoc</title>
+			<!-- needed for adaptive design -->
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+		
+			<!--
+			Redoc doesn't change outer page styles
+			-->
+			<style>
+			  body {
+				margin: 0;
+				padding: 0;
+			  }
+			</style>
+		  </head>
+		  <body>
+			<redoc spec-url='http://localhost:4000/v0/spec.json'></redoc>
+			<script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"> </script>
+		  </body>
+		</html>`))
+	})
+
+	r.Get("/v0/spec.json", func(w http.ResponseWriter, r *http.Request) {
+		swagger, err := GetSwagger()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		json, err := json.Marshal(swagger)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Write(json)
+	})
+
+	handler := Handler(f, WithRouter(r), WithMiddleware("session", f.sessionMiddleware), WithServerBaseURL("/v0"))
 
 	go func() {
 		log.Info().Msgf("listening on http://localhost:%s/", c.Port)
