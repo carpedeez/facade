211bff13e884db4e7fb758062457d84fe3ebbcb9
Henry Lu <hyl2415@rit.edu>
2022-08-23 23:28:52 -0400

    fixed db script

--- scripts/db.sh
+++ scripts/db.sh
@@ -14,17 +14,17 @@ echo "*** TEMPORARY SCRIPT BEFORE IMPLEMENTING MIGRATIONS ***"
 # DEV_DATABASE=facade
 
 setup() {
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "CREATE USER $DEV_USERNAME WITH PASSWORD '$DEV_PASSWORD'"
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "CREATE DATABASE $DEV_DATABASE"
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "GRANT ALL PRIVILEGES ON DATABASE $DEV_DATABASE TO $DEV_USERNAME"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "CREATE USER $DEV_USERNAME WITH PASSWORD '$DEV_PASSWORD'"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "CREATE DATABASE $DEV_DATABASE"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "GRANT ALL PRIVILEGES ON DATABASE $DEV_DATABASE TO $DEV_USERNAME"
 
-    PGPASSWORD=$DEV_PASSWORD psql --host=$PGHOST --port=$PGPORT --username=$DEV_USERNAME $DEV_DATABASE -f "scripts/setup.sql"
+    PGPASSWORD=$DEV_PASSWORD psql --host=localhost --port=$PGPORT --username=$DEV_USERNAME $DEV_DATABASE -f "scripts/setup.sql"
 }
 
 teardown() {
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DEV_DATABASE' AND pid <> pg_backend_pid()"
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "DROP DATABASE $DEV_DATABASE"
-    PGPASSWORD=$PGPASSWORD psql --host=$PGHOST --username=$PGUSER $PGDATABASE -c "DROP USER $DEV_USERNAME"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DEV_DATABASE' AND pid <> pg_backend_pid()"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "DROP DATABASE $DEV_DATABASE"
+    PGPASSWORD=$PGPASSWORD psql --host=localhost --username=$PGUSER $PGDATABASE -c "DROP USER $DEV_USERNAME"
 }
 
 case "$1" in
